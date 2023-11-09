5c31eea6d793dbbe66b45e14672e0ea2e2d00a84
Malika <mkkk2000@hw.ac.uk>
2023-11-10 00:48:53 +0400

    done

--- week4/cities/main.js
+++ week4/cities/main.js
@@ -1,37 +1,35 @@
 var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
-btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://ha2051.github.io/halaqudsi/week4/cities/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-btn.classList.add("hide-me");
-};
-ourRequest.send();
+
+btn.addEventListener("click", function () {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://mkkk2000.github.io/F28WP.lab/week4/cities/cities1.json');
+  ourRequest.onload = function () {
+    if (ourRequest.status >= 200 && ourRequest.status < 300) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+      btn.classList.add("hide-me");
+    } else {
+      alert(`HTTP error! Status: ${ourRequest.status}`);
+    }
+  };
+  ourRequest.send();
 });
-function renderHTML(data){
-var htmlString = "";
-for (i=0; i<data.length; i++){
-htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: " ;
-for (ii = 0; ii < data[i].places.indoor.length; ii++) {
-// Loop through the indoor places of the current city.
-if (ii == 0) {
-htmlString += data[i].places.indoor[ii];
-} else {
-htmlString += ", and " + data[i].places.indoor[ii];
-}
-}
-htmlString += '. & enjoy outdoor places like: ';
-// Loop through the outdoor places of the current city.
-for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
-if (ii == 0) {
-htmlString += data[i].places.outdoor[ii];
-} else {
-htmlString += " and " + data[i].places.outdoor[ii];
-}
-}
-htmlString += '.</p>';
-}
-cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+
+function renderHTML(data) {
+  var htmlString = "";
+  for (var i = 0; i < data.length; i++) {
+    htmlString += `<p>${data[i].name} is a city in ${data[i].country},</br> Where you can enjoy indoor places like: `;
+    for (var ii = 0; ii < data[i].places.indoor.length; ii++) {
+      // Loop through the indoor places of the current city.
+      htmlString += ii === 0 ? data[i].places.indoor[ii] : `, and ${data[i].places.indoor[ii]}`;
+    }
+    htmlString += '. & enjoy outdoor places like: ';
+    // Loop through the outdoor places of the current city.
+    for (var ii = 0; ii < data[i].places.outdoor.length; ii++) {
+      htmlString += ii === 0 ? data[i].places.outdoor[ii] : ` and ${data[i].places.outdoor[ii]}`;
+    }
+    htmlString += '.</p>';
+  }
+  cityContainer.insertAdjacentHTML('beforeend', htmlString);
 }
--- week4/weather/app.js
+++ week4/weather/app.js
@@ -3,12 +3,10 @@ const apiKey = '2e0bb3b26408a54823e7ddf16932990f';
 
 
 const inputField = document.getElementById('cityInput');
-const searchButton = document.getElementById('btn'); 
-const weatherInfoDiv = document.getElementById('weather-info'); 
+const searchButton = document.getElementById('btn');
+const weatherInfoDiv = document.getElementById('weather-info');
 
-
-searchButton.addEventListener('click', () => {
-  
+searchButton.addEventListener('click', async () => {
   const cityName = inputField.value.trim();
 
   if (cityName === '') {
@@ -16,38 +14,27 @@ searchButton.addEventListener('click', () => {
     return;
   }
 
-  
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  try {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
 
-  const request = new XMLHttpRequest();
+    if (response.ok) {
+      const data = await response.json();
 
-  request.open('GET', apiUrl, true);
-
-  request.onload = function () {
-    
-    if (request.status >= 200 && request.status < 300) {
-      const data = JSON.parse(request.responseText);
-      
       const weatherDescription = data.weather[0].description;
       const mainTemperature = data.main.temp;
       const windSpeed = data.wind.speed;
 
       weatherInfoDiv.innerHTML = `
-      <p>City Name: ${cityName}</p>
+        <p>City Name: ${cityName}</p>
         <p>Weather: ${weatherDescription}</p>
         <p>Main Temperature: ${mainTemperature} K</p>
         <p>Wind Speed: ${windSpeed} m/s</p>
       `;
     } else {
-      
-      alert(`HTTP error! Status: ${request.status}`);
+      alert(`HTTP error! Status: ${response.status}`);
     }
-  };
-
-  request.onerror = function () {
-    
-    alert('Network error. Please check your internet connection');
-  };
-
-  request.send();
+  } catch (error) {
+    alert('Network error or JSON parsing error. Please check your internet connection');
+  }
 });
